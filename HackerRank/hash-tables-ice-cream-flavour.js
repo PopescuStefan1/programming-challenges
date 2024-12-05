2c686c2f00e86adbfaa2534038b250213388474b
PopescuStefan1 <stefan.ro95@gmail.com>
2024-12-05 17:19:00 +0200

    Add hash-tables-ice-cream-flavour problem

---
+++ HackerRank/hash-tables-ice-cream-flavour.js
@@ -0,0 +1,62 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'whatFlavors' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY cost
+ *  2. INTEGER money
+ */
+
+function whatFlavors(cost, money) {
+  // Write your code here
+  for (let i = 0; i < cost.length - 1; i++) {
+    if (cost[i] >= money) {
+      continue;
+    }
+
+    for (let j = i + 1; j < cost.length; j++) {
+      if (cost[i] + cost[j] === money) {
+        console.log(`${i + 1} ${j + 1}`);
+        return;
+      }
+    }
+  }
+}
+
+function main() {
+  const t = parseInt(readLine().trim(), 10);
+
+  for (let tItr = 0; tItr < t; tItr++) {
+    const money = parseInt(readLine().trim(), 10);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const cost = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((costTemp) => parseInt(costTemp, 10));
+
+    whatFlavors(cost, money);
+  }
+}
