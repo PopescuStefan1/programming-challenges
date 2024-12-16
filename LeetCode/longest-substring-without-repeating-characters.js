81aa2dd72fb8c5b136f78378e591a838968f1240
PopescuStefan1 <stefan.ro95@gmail.com>
2024-12-16 18:29:36 +0200

    Improve solution to longest-substring-without-repeating-characters problem

--- LeetCode/longest-substring-without-repeating-characters.js
+++ LeetCode/longest-substring-without-repeating-characters.js
@@ -7,19 +7,38 @@ var lengthOfLongestSubstring = function (s) {
     return 1;
   }
 
-  const charMap = new Map();
-  charMap.set(s[0], 1);
+  // Initial solution using hashMap:
+  //   const charMap = new Map();
+  //   charMap.set(s[0], 1);
+
+  //   let longest = 0;
+  //   let left = 0;
+  //   let right = 1;
+  //   while (right < s.length) {
+  //     while (charMap.has(s[right])) {
+  //       charMap.delete(s[left]);
+  //       left++;
+  //     }
+
+  //     charMap.set(s[right], 1);
+
+  //     longest = right - left + 1 > longest ? right - left + 1 : longest;
+  //     right++;
+  //   }
+
+  // Improved solution using set:
+  const charSet = new Set([s[0]]);
 
   let longest = 0;
   let left = 0;
   let right = 1;
   while (right < s.length) {
-    while (charMap.has(s[right])) {
-      charMap.delete(s[left]);
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
       left++;
     }
 
-    charMap.set(s[right], 1);
+    charSet.add(s[right]);
 
     longest = right - left + 1 > longest ? right - left + 1 : longest;
     right++;
