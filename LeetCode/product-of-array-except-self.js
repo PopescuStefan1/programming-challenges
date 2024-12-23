75d8d01e761cfc914839020573f1f77b0c2ff656
PopescuStefan1 <stefan.ro95@gmail.com>
2024-12-23 19:29:04 +0200

    Update product-of-array-except-self solution

--- LeetCode/product-of-array-except-self.js
+++ LeetCode/product-of-array-except-self.js
@@ -3,19 +3,43 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  const result = [];
+  // First solution:
+  // const prefixes = Array(nums.length);
+  // const suffixes = Array(nums.length);
 
-  let prefix = 1;
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = prefix;
+  // prefixes[0] = nums[0];
+  // for(let i = 1; i < nums.length; i++) {
+  //     prefixes[i] = prefixes[i - 1] * nums[i];
+  // }
+
+  // suffixes[nums.length - 1] = nums[nums.length - 1];
+  // for(let i = nums.length - 2; i >= 0; i--) {
+  //     suffixes[i] = suffixes[i + 1] * nums[i];
+  // }
+
+  // const results = Array(nums.length);
+  // results[0] = suffixes[1];
+  // results[nums.length - 1] = prefixes[nums.length - 2];
+  // for(let i = 1; i < nums.length - 1; i++) {
+  //     results[i] = prefixes[i - 1] * suffixes[i + 1];
+  // }
+
+  // return results;
+
+  // Second solution:
+  const results = Array(nums.length);
+  let prefix = nums[0];
+  results[0] = 1;
+  for (let i = 1; i < nums.length; i++) {
+    results[i] = prefix;
     prefix *= nums[i];
   }
 
-  let suffix = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
-    result[i] *= suffix;
+  let suffix = nums[nums.length - 1];
+  for (let i = nums.length - 2; i >= 0; i--) {
+    results[i] *= suffix;
     suffix *= nums[i];
   }
 
-  return result;
+  return results;
 };
