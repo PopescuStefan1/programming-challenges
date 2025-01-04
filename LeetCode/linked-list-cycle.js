5ace72f21c67ff633fa97c23ab0bb7b3f3052647
PopescuStefan1 <stefan.ro95@gmail.com>
2025-01-04 16:07:29 +0200

    Add linked-list-cycle problem

---
+++ LeetCode/linked-list-cycle.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  // Use a slow pointer and a fast pointer
+  // If the fast pointer catches up to the slow pointer at any point then there is a cycle
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
